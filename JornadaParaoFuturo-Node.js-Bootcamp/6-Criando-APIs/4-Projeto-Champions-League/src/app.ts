90ee6206236d07e7a792b40b5bcb1191d837b8a7
Patricia Zan <patricia.zan.oliveira@gmail.com>
2026-03-04 13:53:52 -0300

    Finalizando projeto Champios-League e adicionando clubs GET | Cors

--- JornadaParaoFuturo-Node.js-Bootcamp/6-Criando-APIs/4-Projeto-Champions-League/src/app.ts
+++ JornadaParaoFuturo-Node.js-Bootcamp/6-Criando-APIs/4-Projeto-Champions-League/src/app.ts
@@ -1,10 +1,18 @@
 import express, { json } from "express";
 import router from "./routes";
+import cors from "cors";
 
 function createApp() {
   const app = express();
   app.use(json());
   app.use("/api", router);
+
+  const corsOptions = {
+    origin: ["http://league.com", "http://champion.com"],
+    methods: ["GET"],
+  };
+
+  app.use(cors(corsOptions)); // aqui configura o url inicial 'www.'
 
   return app;
 }
--- JornadaParaoFuturo-Node.js-Bootcamp/6-Criando-APIs/4-Projeto-Champions-League/src/routes.ts
+++ JornadaParaoFuturo-Node.js-Bootcamp/6-Criando-APIs/4-Projeto-Champions-League/src/routes.ts
@@ -6,6 +6,7 @@ import {
   postPlayer,
   updatePlayer,
 } from "./controllers/players-controller";
+import { getClubs } from "./controllers/clubs-controller";
 
 const router = Router();
 
@@ -13,7 +14,8 @@ router.get("/players", getPlayer);
 router.post("players", postPlayer);
 router.delete("/players/:id", deletePlayer);
 router.patch("/players/:id", updatePlayer);
-
 router.get("/players/:id", getPlayerById);
+
+router.get("./clubs", getClubs);
 
 export default router;
